0d0a96ba865011c02f2bf9dad6340ea7fc11666e
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-03-08 20:16:24 +0200

    adding the basic structure for the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return <nav className="fixed bg-slate-950/20 backdrop-blur-sm top-0 z-50 w-full transition-all duration-300">
+        <div className="min-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between h-14 sm:h-16 md:h-20">
+                
+            </div>
+        </div>
+    </nav>;
+}
+
