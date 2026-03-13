c7bf97df9ffcb42d68834fe86796ff09e68bd572
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-03-14 01:27:40 +0200

    typo edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export default function Navbar() {
           <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
             <a href="#features" className="text-gray-400 font-bold hover:text-white text-sm lg:text-base">Features</a>
             <a href="#pricing" className="text-gray-400 font-bold hover:text-white text-sm lg:text-base">Pricing</a>
-            <a href="#testimonials" className="text-gray-400 font-bold hover:text-white text-sm lg:text-base">Cients-Rate</a>
+            <a href="#testimonials" className="text-gray-400 font-bold hover:text-white text-sm lg:text-base">Clients-Rate</a>
           </div>
 
           <button
