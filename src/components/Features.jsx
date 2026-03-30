ceb452b7dfb6b9d9ff36f70a5fcfaf05f9cec503
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-03-30 06:08:30 +0200

    all the project is done

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -112,10 +112,13 @@ export default function Features() {
               {/* text section */}
               <div className="flex-1 w-full">
                 <div className="max-w-lg mx-auto lg:mx-0 text-center lg:text-left">
-                  <h3 className="text-4xl sm:text-3xl lg:text-4xl font-bold mb-4 sm:mb-6 text-white">
+                  <h3 className="text-4xl sm:text-3xl 
+                  lg:text-4xl font-bold mb-4 sm:mb-6
+                   text-white">
                     {feature.title}
                   </h3>
-                  <p className="text-gray-300 text-base  sm:text-lg leading-relaxed">
+                  <p className="text-gray-300 text-base 
+                   sm:text-lg leading-relaxed">
                     {feature.description}
                   </p>
                 </div>
